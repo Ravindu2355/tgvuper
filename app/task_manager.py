64727628c4bc4981cfc88aecf1a8f8d0cb58704a
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 16:50:19 +0530

    Update task_manager.py

--- app/task_manager.py
+++ app/task_manager.py
@@ -17,24 +17,25 @@ def add_task_to_list(url, chat_id, thumbnail_url=None):
     task_list.append(task)
 
 # Function to process tasks from the task list
-def process_tasks(client):
+async def process_tasks(client):
     while True:
         if task_list:
             task = task_list.pop(0)  # Get the first task in the list
             url = task['url']
             chat_id = task['chat_id']
             thumbnail_url = task['thumbnail_url']
-            
+            msg= await client.send_message(chat_id,"Starting Task!")
             # Download the file from the URL
-            file_path = download_file(client, url, "downloaded_file", chat_id)
+            file_path = download_file(client, msg, url, "downloaded_file", chat_id)
             if file_path:  # Only upload if the file was successfully downloaded
                 # Upload the file to Telegram
-                upload_file_to_telegram(client, task, file_path)
+                upload_file_to_telegram(client, msg, task, file_path)
             
                 # Clean up downloaded file after upload
                 if os.path.exists(file_path):
                     os.remove(file_path)
-        
+                    await msg.delete()
+    
         time.sleep(2)  # Wait 2 seconds before checking again
 
 # Start the task processing in a background thread
