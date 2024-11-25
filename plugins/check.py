264d3d4aba6a4a16fb8c0fd8943ae7b3cf6f2d1d
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 18:05:33 +0530

    Update check.py

--- plugins/check.py
+++ plugins/check.py
@@ -1,11 +1,11 @@
-from pyrogram import filters
+from pyrogram import Client, filters, types
+
 
 # Handler for the /check command
+@Client.on_message(filters.command("check"))
 async def check_handler(client, message):
-    if message.text.startswith("/check"):
-        # Check the number of tasks in the task list
-        if task_list:
-            task_count = len(task_list)
-            await client.send_message(message.chat.id, f"There are {task_count} tasks in the queue.")
-        else:
-            await client.send_message(message.chat.id, "There are no tasks in the queue.")
+   if task_list:
+         task_count = len(task_list)
+         await client.send_message(message.chat.id, f"There are {task_count} tasks in the queue.")
+   else:
+         await client.send_message(message.chat.id, "There are no tasks in the queue.")
