4d10c2adf90059ac95a862e533e74667c1c28d4c
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 18:32:33 +0530

    Update url_add.py

--- plugins/url_add.py
+++ plugins/url_add.py
@@ -4,12 +4,12 @@ from task_manager import add_task_to_list
 # Handler for the /url_add command
 @Client.on_message(filters.regex("url_add"))
 async def url_add_handler(client, message):
-        parts = message.text.split(" ", 2)
-        if len(parts) < 3:
+        parts = message.text.split(" ")
+        if len(parts) < 2:
             await client.send_message(message.chat.id, "Please provide a URL and optional thumbnail: /url_add <url> <thumbnail_url>")
             return
         url = parts[1]
-        thumbnail_url = parts[2] if len(parts) > 2 else None
+        thumbnail_url = None #parts[2] if len(parts) > 2 else None
         # Add the task to the task list
         add_task_to_list(url, message.chat.id, thumbnail_url)
         await client.send_message(message.chat.id, f"Task added for URL: {url}")
