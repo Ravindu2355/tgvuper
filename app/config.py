34be4c541a37956a60af29d4450bc25006274429
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 16:05:22 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
+import os
+
 # Configuration settings for your bot
-API_ID = 'YOUR_API_ID'  # Get from https://my.telegram.org/auth
-API_HASH = 'YOUR_API_HASH'  # Get from https://my.telegram.org/auth
-BOT_TOKEN = 'YOUR_BOT_TOKEN'  # Get from @BotFather
+API_ID = os.getenv('apiid')  # Get from https://my.telegram.org/auth
+API_HASH = os.getenv('apihash')  # Get from https://my.telegram.org/auth
+BOT_TOKEN = os.getenv('tk)  # Get from @BotFather
 
 # Flask server settings
 FLASK_HOST = '0.0.0.0'
