7776b450aa6d55312f6ac7bc5065f067f91f5688
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 16:21:28 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,8 @@
-import os
+import os, time
 import requests
 from mimetypes import guess_type
 from moviepy.editor import VideoFileClip
+from up_progress import progress_for_pyrogram
 
 # Function to generate a thumbnail from the video if no thumbnail is provided
 def generate_thumbnail(video_path, thumbnail_path="thumbnail.jpg"):
@@ -16,12 +17,14 @@ def generate_thumbnail(video_path, thumbnail_path="thumbnail.jpg"):
         return None
 
 # Function to download file from the URL with progress
-def download_file(url, download_path, chat_id):
+async def download_file(url, download_path, chat_id):
     try:
+        msg = await client.send_message(chat_id,"starting download...")
         response = requests.get(url, stream=True)
         total_size = int(response.headers.get('content-length', 0))
         downloaded = 0
-        
+        start_t=time.time()
+        old_pm=""
         with open(download_path, "wb") as file:
             for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
@@ -29,9 +32,16 @@ def download_file(url, download_path, chat_id):
                     downloaded += len(chunk)
                     progress = (downloaded / total_size) * 100 if total_size > 0 else 0
                     progress_message = f"Downloading... {progress:.2f}%"
-                    client.send_message(chat_id, progress_message)  # Send progress message to user
+                    now=time.time()
+                    diff = now - start_t
+                    pm=progress_message
+                    if round(diffr % 10.00) == 0 or downloaded_size == total_size:
+                       if old_pm != pm:
+                           await msg.edit_text(pm)  # Send progress message to user
         return download_path
+        await msg.delete()
     except Exception as e:
+        await client.send_message(chat_id,f"Err on url:{url}\ndl Err: {e}")
         return None
 
 # Function to upload file to Telegram (with optional thumbnail) with progress
@@ -42,6 +52,7 @@ def upload_file_to_telegram(task, file_path):
         client.send_message(task['chat_id'], progress_message)  # Send progress message to user
 
     try:
+        msg = await client.send_message(chat_id,"Starting Upload...")
         if file_path:
             file_type = guess_type(file_path)[0]
             thumbnail_path = None
