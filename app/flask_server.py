0f1ed8d1b231147c695542f74dd0aa99e47929ef
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 16:01:33 +0530

    Update flask_server.py

--- app/flask_server.py
+++ app/flask_server.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
-from app.bot import add_task_to_list
-from app.config import FLASK_HOST, FLASK_PORT
+from app.task_manager import add_task_to_list
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Flask route to handle adding tasks via POST request
 @app.route('/add_task', methods=['POST'])
 def add_task():
     data = request.json
@@ -14,12 +12,10 @@ def add_task():
     thumbnail_url = data.get('thumbnail_url', None)
     
     if url and chat_id:
-        # Add the task to the bot's task list
         add_task_to_list(url, chat_id, thumbnail_url)
         return jsonify({"status": "success", "message": f"Task added for URL: {url}"}), 200
     else:
         return jsonify({"status": "error", "message": "Missing url or chat_id"}), 400
 
-# Run the Flask server
 if __name__ == '__main__':
-    app.run(host=FLASK_HOST, port=FLASK_PORT)
+    app.run(host='0.0.0.0', port=8000)
