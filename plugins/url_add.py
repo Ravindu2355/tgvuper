ab9514d392743d9a19a812c7fa4cf7343557d134
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-26 07:03:37 +0530

    Update url_add.py

--- plugins/url_add.py
+++ plugins/url_add.py
@@ -5,8 +5,9 @@ from config import authU
 # Handler for the /url_add command
 @Client.on_message(filters.regex("url_add"))
 async def url_add_handler(client, message):
-        parts = message.text.split(" ")
-        if len(parts) < 2 & str(message.chat.id) in authU:
+     parts = message.text.split(" ")
+     if str(message.chat.id) in authU:
+        if len(parts) < 2:
             await client.send_message(message.chat.id, "Please provide a URL and optional thumbnail: /url_add <url> <thumbnail_url>")
             return
         url = parts[1]
@@ -14,6 +15,8 @@ async def url_add_handler(client, message):
         # Add the task to the task list
         add_task_to_list(url, message.chat.id, thumbnail_url)
         await client.send_message(message.chat.id, f"Task added for URL: {url}")
+     else:
+        await client.send_message(message.chat.id,"Sorry You are not user😄")
 
 @Client.on_message(filters.regex("http"))
 async def p_url(client,message):
