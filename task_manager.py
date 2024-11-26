947457176fb50ca0ef4e4d0f7d97fa37ef26a726
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-26 15:14:27 +0530

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -41,7 +41,7 @@ async def process_tasks(client):
             url = task['url']
             chat_id = task['chat_id']
             thumbnail_url = task['thumbnail_url']
-            if 'page' in task['type']:
+            if task['type'] and page' in task['type']:
                     msg= await client.send_message(chat_id,"Starting page Extract!")
                     exd = ex_page(task)
                     await msg.edit_text(f"Extracted: {len(exd)} sources from that page!...")
