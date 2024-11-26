fe381e3709805e1f70fe92b71ebf6c22eb150619
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-26 16:34:19 +0530

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from task_manager import add_task_to_list, get_task_list
 from flask_cors import CORS
 from threading import Thread
-import os, asyncio, json
+import os, asyncio
 import globals
 # Initialize the Flask application
 app = Flask(__name__)
