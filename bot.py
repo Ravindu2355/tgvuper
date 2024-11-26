78f6ec6310a8cd897a776ef4570fa2634ff6e552
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-26 10:15:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from plugins.url_add import url_add_handler
 from plugins.check import check_handler
 from config import API_ID, API_HASH, BOT_TOKEN
 from task_manager import process_tasks, start_task_processing
+from flask_server import start_fls
 
 # Initialize the Pyrogram Client
 plugins = dict(root="plugins")
@@ -14,6 +15,8 @@ bot = Client(name="RVX_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HA
 # Start task processing in the background
 start_task_processing(bot)
 
+
+
 @bot.on_message(filters.command("start"))
 async def _ms(client, message:types.Message):
     await message.reply("okkk")
@@ -21,6 +24,8 @@ async def _ms(client, message:types.Message):
 #bot.add_handler(url_add_handler)
 #bot.add_handler(check_handler)
 
+flask_t= Thread(target=start_fls)
+flask_t.start()
 # Run the bot
 if __name__ == '__main__':
     bot.run()
