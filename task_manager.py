58498bb5965cebf585fe66dd21f3d4a3d8fc6ac1
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-26 06:24:05 +0530

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -11,11 +11,12 @@ running=0
 s=0
 
 # Function to add a task to the task list
-def add_task_to_list(url, chat_id, thumbnail_url=None):
+def add_task_to_list(url, chat_id, thumbnail_url=None, type=None):
     task = {
         'url': url,
         'chat_id': chat_id,
-        'thumbnail_url': thumbnail_url
+        'thumbnail_url': thumbnail_url,
+        'type':type
     }
     task_list.append(task)
 
