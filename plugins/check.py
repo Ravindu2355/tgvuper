27647ff71e9d0e588c143364e9f1305acf30f8f7
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-26 11:12:52 +0530

    Update check.py

--- plugins/check.py
+++ plugins/check.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters, types
 from task_manager import  s
-from globals import task_list
+from globals
 
 # Handler for the /check command
 @Client.on_message(filters.command("check"))
 async def check_handler(client, message):
-   if task_list:
-         task_count = len(task_list)
+   if globals.task_list:
+         task_count = len(globals.task_list)
          await client.send_message(message.chat.id, f"There are {task_count} tasks in the queue.And process fun runed {s} times")
    else:
          await client.send_message(message.chat.id, "There are no tasks in the queue.")
