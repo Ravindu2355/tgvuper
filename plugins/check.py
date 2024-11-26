aa0e1f38fbed9236dc2b9e0de759dd23ff79e8fd
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-26 11:06:58 +0530

    Update check.py

--- plugins/check.py
+++ plugins/check.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters, types
-from task_manager import task_list, s
+from task_manager import  s
+from globals import task_list
 
 # Handler for the /check command
 @Client.on_message(filters.command("check"))
