fd9c04bba6b50c7f7c6ffab8eb0b3dd02652021d
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-30 06:24:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,12 @@ API_ID = os.getenv('apiid')  # Get from https://my.telegram.org/auth
 API_HASH = os.getenv('apihash')  # Get from https://my.telegram.org/auth
 BOT_TOKEN = os.getenv('tk')  # Get from @BotFather
 authU = os.getenv('auth')
-sl_time = int(os.getenv('sleep'))
+sleep_time = int(os.getenv('sleep') or 30)
+try:
+    sleep_time = int(sleep_time)
+except ValueError:
+    sleep_time = 30
+
 # Flask server settings
 FLASK_HOST = '0.0.0.0'
 FLASK_PORT = 8000
