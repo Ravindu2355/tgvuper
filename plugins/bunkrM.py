f4bfadf16439a7067a2ff45997cb92750fd9dda9
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-02-18 19:01:42 +0530

    Update bunkrM.py

--- plugins/bunkrM.py
+++ plugins/bunkrM.py
@@ -36,7 +36,8 @@ def extract_video_urls(page_url):
         response = requests.get(page_url)
         response.raise_for_status()
         html_content = response.text
-        mts = list(set(re.findall(video_url_pattern, html_content)))  # Remove duplicates
+        #mts = list(set(re.findall(video_url_pattern, html_content)))  # Remove duplicates
+        mts = list(set(match[0] for match in re.findall(video_url_pattern, html_content)))  # Extract only full matches
         video_urls = transform_links(mts, baseu)
         return video_urls
     except requests.RequestException as e:
@@ -66,9 +67,10 @@ async def bunkr_bulk(client, message):
         
         # Step 1: Extract video URLs
         video_urls = extract_video_urls(page_url)
+        baseu = get_base_url(page_url)
         
         if video_urls is None:
-            await message.reply("Error extracting URLs. Please check the provided URL.")
+            await message.reply(f"Error extracting URLs. Please check the provided URL.\nbase URL :{baseu}")
             return
         
         if not video_urls:
@@ -82,7 +84,7 @@ async def bunkr_bulk(client, message):
         task_list.extend(payload_array)
         
         # Show the updated task list
-        await message.reply(f"**Task list updated!\n\nAdd: {len(payload_array)} videos.\n\nChat ID: {chat_id}\n\nTask List:\n{len(task_list)} videos**")
+        await message.reply(f"**Task list updated!\n\nAdd: {len(payload_array)} videos.\n\nChat ID: {chat_id}\n\nTask List:\n{len(task_list)} videos**\nbase URL :{baseu}\n 1st:{video_urls[0]}")
         
     except Exception as e:
         await message.reply(f"An error occurred: {str(e)}")
