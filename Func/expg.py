f78e6cf23ae68936ea3489ec9a208fa383953ae7
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-08-22 12:59:44 +0530

    Update expg.py

--- Func/expg.py
+++ Func/expg.py
@@ -6,7 +6,12 @@ from log import logger as lg
 from urllib.parse import urlparse
 
 s_h = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
 }
 s_c={}
 
