5147dc428e95cbf761a3bcbcbca2c01cde753c66
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-09-08 06:55:09 +0530

    Update url_add.py

--- plugins/url_add.py
+++ plugins/url_add.py
@@ -2,6 +2,20 @@ from pyrogram import Client, filters, types
 from task_manager import add_task_to_list, start_task_processing
 from config import authU
 
+@Client.on_message(filters.regex("yt"))
+async def url_add_handler_yt(client, message):
+     parts = message.text.split(" ")
+     if str(message.chat.id) in authU:
+        if len(parts) < 2:
+            await client.send_message(message.chat.id, "Please provide a URL and optional thumbnail: /url_add <url> <thumbnail_url>")
+            return
+        url = parts[1]
+        type="yt_direct"
+        add_task_to_list(url, message.chat.id, thumbnail_url = None,type=type)
+        await client.send_message(message.chat.id, f"Task added for URL: {url}")
+     else:
+        await client.send_message(message.chat.id,"Sorry You are not user😄")
+          
 # Handler for the /url_add command
 @Client.on_message(filters.regex("url_add"))
 async def url_add_handler(client, message):
