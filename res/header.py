f8e88ffda35f777b4087ed1d9e7521b528b0b49e
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-11-19 05:52:13 +0530

    Update header.py

--- res/header.py
+++ res/header.py
@@ -3,18 +3,13 @@ import os
 s_h = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
-
 sites={
-        
-}
-
-"""sites={
   "bunkr":{
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     "Referer": "https://bunkr.cr/"
   },
   
-}"""
+}
 
 def get_h(url):
     for k in sites:
